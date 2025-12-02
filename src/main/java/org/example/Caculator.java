a4c9bb3da2f871f7e18f7f5ea13ad16736109f17
redbee <nhnhaminh2008@gmail.com>
2025-12-02 13:10:17 +0700

    first commit

---
+++ src/main/java/org/example/Caculator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Caculator implements Icaculator{
+    public Integer sum(Integer a, Integer b){
+        return 0;
+    }
+    public Integer sub(Integer a, Integer b){
+        return 0;
+    }
+}
